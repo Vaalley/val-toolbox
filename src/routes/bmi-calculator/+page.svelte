e05171c957ca05d41791385d5d80edb17e07ce37
Valley <vaaleyou@gmail.com>
2023-10-26 00:21:42 +0200

    Add BMI calculation result to UI

--- src/routes/bmi-calculator/+page.svelte
+++ src/routes/bmi-calculator/+page.svelte
@@ -19,13 +19,13 @@
 <main class="container mx-auto flex flex-col items-center">
 	<h1 class="h1 mt-16 font-bold">BMI Calculator</h1>
 	<div class="mt-16 card p-5 w-fit space-y-5 min-w-[400px]">
+		<h2 class="h2 font-medium">Your BMI is: {bmi}</h2>
 		<input bind:value={weight} class="input" placeholder="Enter your weight in kg" type="text" />
 		<input bind:value={height} class="input" placeholder="Enter your height in cm" type="text" />
 		<div class="flex gap-5">
 			<button on:click={() => calculateBMI(weight, height)} class="btn variant-filled"
 				>Calculate</button
 			>
-			<p>Your BMI is: {bmi}</p>
 		</div>
 	</div>
 	<a
