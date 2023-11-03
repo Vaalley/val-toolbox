1d337eb13f65425df1cd87bd5682d12d4a4bffd5
Valley <vaaleyou@gmail.com>
2023-11-03 18:18:02 +0100

    feat(weather): Add weather display functionality
    - Adds functionality to display weather information for a specific location.
    - Implements the ability to retrieve weather data using the OpenWeatherMap API.
    - Enables users to enter their desired location and choose between metric and imperial units.
    - Displays various weather details such as country, description, wind, humidity, temperature, clouds, and coordinates.
    - Includes a link to Google Maps for the specified location.
    - Provides pressure information in hPa.

--- src/routes/weather/+page.svelte
+++ src/routes/weather/+page.svelte
@@ -1,4 +1,20 @@
 <script>
+	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+
+	const apiKey = '1587159bb0692bc32c752afb5ebd8948';
+	let city;
+	let unit = 0;
+	let data;
+
+	async function getWeather() {
+		const unitParam = unit === 0 ? 'metric' : 'imperial';
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unitParam}`
+		);
+		data = await response.json();
+
+		console.log(data);
+	}
 </script>
 
 <svelte:head>
@@ -8,7 +24,76 @@
 <main class="container mx-auto flex flex-col items-center">
 	<h1 class="h1 mt-16 font-bold">Weather</h1>
 	<div class="mt-16 card p-5 w-fit space-y-5 min-w-[400px]">
-		<h2 class="h2 font-medium">Weather for your location is:</h2>
+		<h2 class="h2 font-medium">Weather for {city ? city : 'your location'} is:</h2>
+		<div class="flex gap-3">
+			<input
+				bind:value={city}
+				class="input w-fit"
+				type="text"
+				placeholder="Enter your location"
+				on:keydown={(event) => {
+					if (event.key === 'Enter') {
+						getWeather();
+					}
+				}}
+			/>
+			<button on:click={getWeather} class="btn variant-filled">🌞</button>
+			<RadioGroup>
+				<RadioItem bind:group={unit} name="unit" value={0}>Metric</RadioItem>
+				<RadioItem bind:group={unit} name="unit" value={1}>Imperial</RadioItem>
+			</RadioGroup>
+		</div>
+		{#if data}
+			<div class="card p-5 grid grid-cols-2 gap-5">
+				<div>
+					<h3 class="h3 font-medium">🌍 Country:</h3>
+					<p>{data.sys.country}</p>
+					<p>Timezone: {data.timezone}</p>
+				</div>
+				<div>
+					<h3 class="h3 font-medium">🎛️ Description:</h3>
+					<p>{data.weather[0].description}</p>
+				</div>
+				<div>
+					<h3 class="h3 font-medium">💨 Wind:</h3>
+					<p>{data.wind.speed} {unit === 0 ? 'm/s' : 'mph'}</p>
+					<p>{data.wind.deg}°</p>
+				</div>
+				<div>
+					<h3 class="h3 font-medium">💧 Humidity:</h3>
+					<p>{data.main.humidity}%</p>
+				</div>
+				<div>
+					<h3 class="h3 font-medium">🌡️ Temperature:</h3>
+					<p>{data.main.temp}°{unit === 0 ? 'C' : 'F'}</p>
+					<p>Feels like: {data.main.feels_like}°{unit === 0 ? 'C' : 'F'}</p>
+					<p>
+						Min / Max: {data.main.temp_min}°{unit === 0 ? 'C' : 'F'} / {data.main
+							.temp_max}°{unit === 0 ? 'C' : 'F'}
+					</p>
+				</div>
+				<div>
+					<h3 class="h3 font-medium">☁️ Clouds:</h3>
+					<p>{data.clouds.all}%</p>
+				</div>
+				<div>
+					<h3 class="h3 font-medium">📍 Coordinates:</h3>
+					<p>{data.coord.lat}, {data.coord.lon}</p>
+					<p>
+						Link to Google Maps: <a
+							class="anchor"
+							href={`https://www.google.com/maps/search/${data.coord.lat},${data.coord.lon}`}
+							target="_blank"
+							rel="noreferrer">here</a
+						>
+					</p>
+				</div>
+				<div>
+					<h3 class="h3 font-medium">🎚️ Pressure:</h3>
+					<p>{data.main.pressure} hPa</p>
+				</div>
+			</div>
+		{/if}
 	</div>
 	<a
 		class="anchor mt-2 text-sm"
