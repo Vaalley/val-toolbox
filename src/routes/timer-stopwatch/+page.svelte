1e138ac98862c81886bc432aead19d448c0d291a
Valley <vaaleyou@gmail.com>
2023-11-13 01:01:26 +0100

    testing commit on webstorm

--- src/routes/timer-stopwatch/+page.svelte
+++ src/routes/timer-stopwatch/+page.svelte
@@ -1,15 +1,4 @@
 <script>
-	let bmi = 0;
-	let weight;
-	let height;
-
-	function calculateBMI(weight, height) {
-		// Convert height from centimeters to meters
-		const heightInMeters = height / 100;
-
-		// Calculate BMI
-		bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
-	}
 </script>
 
 <svelte:head>
@@ -18,7 +7,9 @@
 
 <main class="container mx-auto flex flex-col items-center">
 	<h1 class="h1 mt-16 font-bold font-outfit">Timer / Stopwatch</h1>
-	<div class="mt-16 card p-5 w-fit space-y-5 min-w-[400px]" />
+	<div class="mt-16 card p-5 w-fit space-y-5 min-w-[400px]">
+		<h2 class="h2 font-outfit">Timer</h2>
+	</div>
 	<a
 		class="anchor mt-2 text-sm"
 		href="https://github.com/Vaalley/val-toolbox/blob/main/src/routes/timer-stopwatch/%2Bpage.svelte"
