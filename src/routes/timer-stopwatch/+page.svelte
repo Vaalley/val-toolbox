7a025c0338870af5bd57f8bec58766089183fee3
Valley <vaaleyou@gmail.com>
2023-11-13 19:54:01 +0100

    Add stopwatch functionality

--- src/routes/timer-stopwatch/+page.svelte
+++ src/routes/timer-stopwatch/+page.svelte
@@ -1,4 +1,30 @@
 <script>
+	import { onMount } from 'svelte';
+
+	let time;
+	let timer;
+	let displayTime;
+
+	$: displayTime = (time / 10000).toFixed(2);
+
+	function startTimer() {
+		timer = setInterval(() => {
+			time += 100;
+			console.log(time);
+		}, 10);
+	}
+
+	function stopTimer() {
+		clearInterval(timer);
+	}
+
+	function resetTimer() {
+		time = 0;
+	}
+
+	onMount(() => {
+		time = 0;
+	});
 </script>
 
 <svelte:head>
@@ -7,8 +33,13 @@
 
 <main class="container mx-auto flex flex-col items-center">
 	<h1 class="h1 mt-16 font-bold font-outfit">Timer / Stopwatch</h1>
-	<div class="mt-16 card p-5 w-fit space-y-5 min-w-[400px]">
-		<h2 class="h2 font-outfit">Timer</h2>
+	<div class="mt-16 card p-5 w-fit min-w-[400px]">
+		<input type="text" class="input h2 font-outfit text-center w-fit" bind:value={displayTime} />
+		<div class="flex justify-around mt-5">
+			<button class="btn variant-filled font-outfit" on:click={startTimer}>Start</button>
+			<button class="btn variant-filled font-outfit" on:click={stopTimer}>Stop</button>
+			<button class="btn variant-filled font-outfit" on:click={resetTimer}>Reset</button>
+		</div>
 	</div>
 	<a
 		class="anchor mt-2 text-sm"
