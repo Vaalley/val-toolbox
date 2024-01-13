807b69c4f7b7c0df264197e316238c98fa04e0e8
Vaalley <vaaleyou@gmail.com>
2024-01-13 17:48:39 +0100

    Updated Welcome Text Tagging
    Improved the semantics of the welcome and instructions text on the home
    page by changing paragraph tags to heading tags for better HTML
    structure and accessibility.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
 
 <main class="container mx-auto flex flex-col items-center">
 	<h1 class="h1 mt-16 font-bold font-outfit">Val's Toolbox</h1>
-	<p class="mt-10">
+	<h5 class="mt-10 h5">
 		Welcome to Val's Toolbox, this website is an assortment of small tools I built for myself and
 		others.
-	</p>
-	<p class="mt-2">Just use the app rail on the left to get started!</p>
+	</h5>
+	<h5 class="mt-2 h5">Just use the app rail on the left to get started!</h5>
 </main>
